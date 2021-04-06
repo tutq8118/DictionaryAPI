b140b6deca33e5d497d84ebfd797f4affb1c8dd7
sanggnguyenn <sang.nguyenngoc@gforces.auto>
2021-04-06 16:26:44 +0700

    add static conf for vercel

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
-app.use('/static', express.static(resolve(__dirname, './public')));
+app.use('/', express.static(resolve(__dirname, './public')));
 
 // view engine setup
 app.set('views', join(__dirname, 'views'));
